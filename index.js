4a3bc8144921bee37491fac86f92800e86f4f695
Ritesh R <72683075+RiteshRavichandran@users.noreply.github.com>
2024-06-05 20:08:00 +0530

    Mobile_Support_V1

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ counters.forEach(counter =>{
     }
     updateCount();
 })
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
