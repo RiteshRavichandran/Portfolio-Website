29907072a1b1eb8fffadcba79ce56b834b502700
Ritesh R <72683075+RiteshRavichandran@users.noreply.github.com>
2024-06-07 17:02:02 +0530

    formatting

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const counters = document.querySelectorAll('.counter');
 const speed = 2000000;
 
-counters.forEach(counter =>{
+counters.forEach(counter => {
     const updateCount = () => {
         const target = +counter.getAttribute('data-target');
         const count = +counter.innerText;
 
         const inc = target / speed;
 
-        if( count < target){
+        if (count < target) {
             counter.innerText = Math.ceil(count + inc);
             setTimeout(updateCount, 1);
-        }else{
+        } else {
             count.innerText = target;
         }
     }
     updateCount();
 })
 
-function showSidebar(){
+function showSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
 }
 
-function hideSidebar(){
+function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
